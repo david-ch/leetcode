f865bb3ff4316f8a1a650a2c91612703485f512d
david-ch <david.ch969@gmail.com>
2023-06-07 19:54:02 +0100

    2095. Delete the Middle Node of a Linked List

---
+++ src/main/java/davidch/leetcode/challenges/n2095/Solution.java
@@ -0,0 +1,46 @@
+package davidch.leetcode.challenges.n2095;
+
+import java.util.Objects;
+
+public class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        final var preHead = new ListNode(0, head);
+
+        var slow = preHead;
+        var fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow.next = slow.next.next;
+        return preHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ListNode listNode = (ListNode) o;
+            return val == listNode.val && Objects.equals(next, listNode.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, next);
+        }
+    }
+}
