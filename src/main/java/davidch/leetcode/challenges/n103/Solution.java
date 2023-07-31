e8da65d1232910a200315fb6e359aac8534c8a7b
david-ch <david.ch969@gmail.com>
2023-07-31 19:30:58 +0100

    103. Binary Tree Zigzag Level Order Traversal

--- src/main/java/davidch/leetcode/challenges/n103/Solution.java
+++ src/main/java/davidch/leetcode/challenges/n103/Solution.java
@@ -39,34 +39,4 @@ public class Solution {
 
         return result;
     }
-
-//    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-//        final var result = new ArrayList<List<Integer>>();
-//        final var currentLevel = new Stack<TreeNode>();
-//        final var nextLevel = new Stack<TreeNode>();
-//
-//        if (root != null) currentLevel.push(root);
-//
-//        while (!currentLevel.isEmpty()) {
-//            final var currentLevelFlat = new ArrayList<Integer>();
-//            while (!currentLevel.isEmpty()) {
-//                final var node = currentLevel.pop();
-//                currentLevelFlat.add(node.val);
-//                if (node.left != null) nextLevel.push(node.left);
-//                if (node.right != null) nextLevel.push(node.right);
-//            }
-//            result.add(currentLevelFlat);
-//
-//            final var nextLevelFlat = new ArrayList<Integer>();
-//            while (!nextLevel.isEmpty()) {
-//                final var node = nextLevel.pop();
-//                nextLevelFlat.add(node.val);
-//                if (node.right != null) currentLevel.push(node.right);
-//                if (node.left != null) currentLevel.push(node.left);
-//            }
-//            if (!nextLevelFlat.isEmpty()) result.add(nextLevelFlat);
-//        }
-//
-//        return result;
-//    }
 }
