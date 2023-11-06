bd63bcbb003c02236f78f5a3125616b151932bd9
david-ch <david.ch969@gmail.com>
2023-11-06 18:11:58 +0000

    1323. Maximum 69 Number

--- src/main/java/davidch/leetcode/challenges/n1323/Solution.java
+++ src/main/java/davidch/leetcode/challenges/n1323/Solution.java
@@ -2,15 +2,14 @@ package davidch.leetcode.challenges.n1323;
 
 public class Solution {
     public int maximum69Number(int num) {
-        var multiplier = 1;
-        var mostSignificant6Multiplier = -1;
+        var multiplier = (int) Math.pow(10, 1 + (int) Math.log10(num));
 
-        while (multiplier < num) {
-            if (num % (multiplier * 10) / multiplier == 6) mostSignificant6Multiplier = multiplier;
-            multiplier *= 10;
+        while (multiplier >= 1) {
+            if (num % (multiplier * 10) / multiplier == 6) break;
+            multiplier /= 10;
         }
 
-        if (mostSignificant6Multiplier != -1) num += mostSignificant6Multiplier * 3;
+        if (multiplier >= 1) num += multiplier * 3;
 
         return num;
     }
