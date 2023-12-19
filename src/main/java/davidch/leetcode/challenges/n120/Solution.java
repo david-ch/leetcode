d03723878d8e6d9632ad9d0e150282129966915f
david-ch <david.ch969@gmail.com>
2023-12-19 09:41:52 +0000

    120. Triangle

--- src/main/java/davidch/leetcode/challenges/n120/Solution.java
+++ src/main/java/davidch/leetcode/challenges/n120/Solution.java
@@ -3,19 +3,26 @@ package davidch.leetcode.challenges.n120;
 import java.util.List;
 
 public class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        for (var row = triangle.size() - 2; row >= 0; row--) {
-            final var r = triangle.get(row);
-            final var nextR = triangle.get(row + 1);
-            final var rowLength = r.size();
 
-            for (var col = 0; col < rowLength; col++)
-                r.set(
-                        col,
-                        r.get(col) + Math.min(nextR.get(col), nextR.get(col + 1))
-                );
+    public int minimumTotal(List<List<Integer>> triangle) {
+        final var memo = new Integer[triangle.size()][triangle.size()];
+
+        final var lastRowNum = triangle.size() - 1;
+        final var lastRow = triangle.get(lastRowNum);
+        for (var col = 0; col < lastRow.size(); col++)
+            memo[lastRowNum][col] = lastRow.get(col);
+
+        return minimumTotal(triangle, 0, 0, memo);
+    }
+
+    private int minimumTotal(List<List<Integer>> triangle, int row, int col, Integer[][] memo) {
+        if (memo[row][col] == null) {
+            memo[row][col] = triangle.get(row).get(col) + Math.min(
+                    minimumTotal(triangle, row + 1, col, memo),
+                    minimumTotal(triangle, row + 1, col + 1, memo)
+            );
         }
 
-        return triangle.get(0).get(0);
+        return memo[row][col];
     }
 }
