e252e0963524ba6505057cfdab8ec573b78295ca
david-ch <david.ch969@gmail.com>
2023-12-21 11:19:11 +0000

    221. Maximal Square

--- src/main/java/davidch/leetcode/challenges/n221/Solution.java
+++ src/main/java/davidch/leetcode/challenges/n221/Solution.java
@@ -1,35 +1,22 @@
 package davidch.leetcode.challenges.n221;
 
-import java.util.Arrays;
-
 public class Solution {
     public int maximalSquare(char[][] matrix) {
-        final var memo = new int[matrix.length][matrix[0].length];
-        for (var row : memo) Arrays.fill(row, -1);
+        final var memo = new int[matrix.length + 1][matrix[0].length + 1];
 
         var result = 0;
 
         for (var row = 0; row < matrix.length; row++)
             for (var col = 0; col < matrix[row].length; col++)
-                result = Math.max(result, maximalSquare(matrix, row, col, memo));
+                if (matrix[row][col] == '1') {
+                    memo[row + 1][col + 1] = 1 + Math.min(
+                            memo[row][col + 1],
+                            Math.min(memo[row + 1][col], memo[row][col])
+                    );
+
+                    result = Math.max(result, memo[row + 1][col + 1]);
+                }
 
         return result * result;
-    }
-
-    public int maximalSquare(char[][] matrix, int startRow, int startCol, int[][] memo) {
-        if (startRow == matrix.length || startCol == matrix[startRow].length || matrix[startRow][startCol] != '1')
-            return 0;
-
-        if (memo[startRow][startCol] == -1) {
-            memo[startRow][startCol] = 1 + Math.min(
-                    Math.min(
-                            maximalSquare(matrix, startRow + 1, startCol + 1, memo),
-                            maximalSquare(matrix, startRow + 1, startCol, memo)
-                    ),
-                    maximalSquare(matrix, startRow, startCol + 1, memo)
-            );
-        }
-
-        return memo[startRow][startCol];
     }
 }
