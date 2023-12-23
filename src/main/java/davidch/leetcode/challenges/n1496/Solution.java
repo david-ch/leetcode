fab0c528f3fa0257be603aed265b7b594c932b01
david-ch <david.ch969@gmail.com>
2023-12-23 12:20:25 +0000

    1496. Path Crossing

--- src/main/java/davidch/leetcode/challenges/n1496/Solution.java
+++ src/main/java/davidch/leetcode/challenges/n1496/Solution.java
@@ -4,27 +4,19 @@ import java.util.HashSet;
 
 public class Solution {
     public boolean isPathCrossing(String path) {
-        final var visitedPoints = new HashSet<Point>();
-        var currentPosition = new Point(0, 0);
-        visitedPoints.add(currentPosition);
+        final var visitedPoints = new HashSet<Long>();
+        var x = 0;
+        var y = 0;
+        visitedPoints.add(0L);
+        for (var ch : path.toCharArray()) {
+            if (ch == 'N') y--;
+            else if (ch == 'S') y++;
+            else if (ch =='E') x++;
+            else x--;
 
-        final var len = path.length();
-        for (var i = 0; i < len; i++) {
-            currentPosition = currentPosition.move(path.charAt(i));
-            if (!visitedPoints.add(currentPosition)) return true;
+            if (!visitedPoints.add(x * 100_000L + y)) return true;
         }
 
         return false;
-    }
-
-    private record Point(int x, int y) {
-        Point move(char direction) {
-            return switch (direction) {
-                case 'N' -> new Point(x, y - 1);
-                case 'S' -> new Point(x, y + 1);
-                case 'E' -> new Point(x + 1, y);
-                default -> new Point(x - 1, y);
-            };
-        }
     }
 }
