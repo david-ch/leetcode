24af97e3f417bdb2fbab50741dff509e6ec007bb
david-ch <david.ch969@gmail.com>
2024-03-14 20:15:57 +0000

    930. Binary Subarrays With Sum

--- src/main/java/davidch/leetcode/challenges/n930/Solution.java
+++ src/main/java/davidch/leetcode/challenges/n930/Solution.java
@@ -1,18 +1,16 @@
 package davidch.leetcode.challenges.n930;
 
-import java.util.HashMap;
-
 public class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        final var seenSumsCount = new HashMap<Integer, Integer>();
-        seenSumsCount.put(0, 1);
+        final var seenSumsCount = new int[nums.length + 1];
+        seenSumsCount[0] = 1;
 
         var result = 0;
         var sum = 0;
         for (var i = 0; i < nums.length; i++) {
             sum += nums[i];
-            result += seenSumsCount.getOrDefault(sum - goal, 0);
-            seenSumsCount.put(sum, seenSumsCount.getOrDefault(sum, 0) + 1);
+            if (sum - goal >= 0) result += seenSumsCount[sum - goal];
+            seenSumsCount[sum]++;
         }
 
         return result;
