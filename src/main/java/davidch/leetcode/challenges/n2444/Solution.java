24bb672c74ff2c1105fe46ff85b4f7042e801513
david-ch <david.ch969@gmail.com>
2024-03-31 12:49:04 +0100

    2444. Count Subarrays With Fixed Bounds

--- src/main/java/davidch/leetcode/challenges/n2444/Solution.java
+++ src/main/java/davidch/leetcode/challenges/n2444/Solution.java
@@ -21,13 +21,17 @@ public class Solution {
             while (!max.isEmpty() && nums[max.peekLast()] <= newNum) max.pollLast();
             max.addLast(right);
 
-            while (!min.isEmpty() && nums[min.peekFirst()] < minK) left = min.pollFirst() + 1;
-            while (!max.isEmpty() && nums[max.peekFirst()] > maxK) left = max.pollFirst() + 1;
-            while (!min.isEmpty() && min.peekFirst() < left) min.pollFirst();
-            while (!max.isEmpty() && max.peekFirst() < left) max.pollFirst();
-
-            if (!min.isEmpty() && !max.isEmpty() && nums[min.peekFirst()] == minK && nums[max.peekFirst()] == maxK)
-                result += Math.min(min.peekFirst(), max.peekFirst()) - left + 1;
+            if (nums[min.peekFirst()] == minK && nums[max.peekFirst()] == maxK) result += Math.min(min.peekFirst(), max.peekFirst()) - left + 1;
+            else if (nums[min.peekFirst()] < minK) {
+                left = min.pollFirst() + 1;
+                while (!min.isEmpty() && min.peekFirst() < left) min.pollFirst();
+                while (!max.isEmpty() && max.peekFirst() < left) max.pollFirst();
+            }
+            else if (nums[max.peekFirst()] > maxK) {
+                left = max.pollFirst() + 1;
+                while (!min.isEmpty() && min.peekFirst() < left) min.pollFirst();
+                while (!max.isEmpty() && max.peekFirst() < left) max.pollFirst();
+            }
         }
 
         return result;
