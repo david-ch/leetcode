527dc2c1e9a5a88bfb5ccc2592b82d71e4bc5b55
david-ch <david.ch969@gmail.com>
2024-05-04 11:38:40 +0100

    1071. Greatest Common Divisor of Strings

--- src/main/java/davidch/leetcode/challenges/n1071/Solution.java
+++ src/main/java/davidch/leetcode/challenges/n1071/Solution.java
@@ -2,27 +2,12 @@ package davidch.leetcode.challenges.n1071;
 
 public class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        final var chars1 = str1.toCharArray();
-        final var chars2 = str2.toCharArray();
-
-        var divisorLen = chars1.length;
-        while (divisorLen > 0) {
-            if (divides(chars2, chars1, divisorLen)) return new String(chars1, 0, divisorLen);
-
-            do divisorLen--;
-            while (divisorLen > 0 && !divides(chars1, chars1, divisorLen));
-        }
-
-        return "";
+        if (!(str1 + str2).equals(str2 + str1)) return "";
+        return str1.substring(0, gcd(str1.length(), str2.length()));
     }
 
-    private boolean divides(char[] str, char[] divisor, int divisorLen) {
-        var divisorI = 0;
-        for (var c : str) {
-            if (c != divisor[divisorI]) return false;
-            divisorI++;
-            if (divisorI == divisorLen) divisorI = 0;
-        }
-        return divisorI == 0;
+    private int gcd(int n1, int n2) {
+        if (n2 == 0) return n1;
+        return gcd(n2, n1 % n2);
     }
 }
